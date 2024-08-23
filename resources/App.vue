67d934f660561290610090eec0ad85e842d13ece
RJK <rjklinkhamer@gmail.com>
2024-08-23 12:32:22 +0200

    me link

--- resources/App.vue
+++ resources/App.vue
@@ -3,6 +3,7 @@
     <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
     <nav>
         <!-- <RouterLink :to="{ name: 'tickets' }">Tickets</RouterLink> -->
+        | <a href="/me">Me</a>
     </nav>
     <main>
         <RouterView />

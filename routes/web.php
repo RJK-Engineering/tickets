ebe8348f8b1bb5ca73d13507275033c16552df6f
RJK <rjklinkhamer@gmail.com>
2024-08-26 12:33:02 +0200

    done for the day

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('app');
+});
+Route::get('/me', function (Request $request) {
+    return $request;
 });

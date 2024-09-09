e6e38d80a36f206a5f7308f185443d3bb2c875fb
RJK <rjklinkhamer@gmail.com>
2024-09-09 12:23:15 +0200

    model

---
+++ app/Http/Controllers/TicketNotesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TicketNotes;
+use Illuminate\Http\Request;
+
+class TicketNotesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TicketNotes $ticketNotes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TicketNotes $ticketNotes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TicketNotes $ticketNotes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TicketNotes $ticketNotes)
+    {
+        //
+    }
+}
---
+++ database/tickets.sql
@@ -0,0 +1,67 @@
+
+CREATE TABLE ticket_categories
+(
+  id   BIGINT  NOT NULL,
+  name VARCHAR NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE ticket_has_category
+(
+  id          BIGINT NOT NULL,
+  ticket_id   BIGINT NOT NULL,
+  category_id BIGINT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE ticket_notes
+(
+  id        BIGINT NOT NULL,
+  ticket_id BIGINT NOT NULL,
+  note      TEXT   NULL    ,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE tickets
+(
+  id              BIGINT   NOT NULL,
+  created_by      BIGINT   NOT NULL,
+  created_at      DATETIME NOT NULL,
+  last_updated_at DATETIME NULL    ,
+  title           VARCHAR  NOT NULL,
+  status          VARCHAR  NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users
+(
+  id         BIGINT  NOT NULL,
+  first_name VARCHAR NOT NULL,
+  last_name  VARCHAR NOT NULL,
+  email      VARCHAR NOT NULL,
+  password   VARCHAR NOT NULL,
+  role       VARCHAR NULL    ,
+  phone      VARCHAR NULL    ,
+  PRIMARY KEY (id)
+);
+
+ALTER TABLE ticket_has_category
+  ADD CONSTRAINT FK_ticket_categories_TO_ticket_has_category
+    FOREIGN KEY (category_id)
+    REFERENCES ticket_categories (id);
+
+ALTER TABLE ticket_has_category
+  ADD CONSTRAINT FK_tickets_TO_ticket_has_category
+    FOREIGN KEY (ticket_id)
+    REFERENCES tickets (id);
+
+ALTER TABLE ticket_notes
+  ADD CONSTRAINT FK_tickets_TO_ticket_notes
+    FOREIGN KEY (ticket_id)
+    REFERENCES tickets (id);
+
+ALTER TABLE tickets
+  ADD CONSTRAINT FK_users_TO_tickets
+    FOREIGN KEY (created_by)
+    REFERENCES users (id);
+

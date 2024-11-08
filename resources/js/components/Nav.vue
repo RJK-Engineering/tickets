a970bcbc2f6cd0bfcfcda9d4b164fd2d2dc1d638
RJK <rjklinkhamer@gmail.com>
2024-11-08 12:31:26 +0100

    done for the day

--- resources/js/components/Nav.vue
+++ resources/js/components/Nav.vue
@@ -18,62 +18,62 @@ console.log(router.getRoutes())
 </template>
 
 <!--
-import {activeStatus, projectStore} from 'domains/projects';
-import {computed} from 'vue';
-import {goToLoginPage, logout} from 'domains/auth';
-import {handStore} from 'domains/hands';
+import {activeStatus, projectStore} from 'domains/projects'
+import {computed} from 'vue'
+import {goToLoginPage, logout} from 'domains/auth'
+import {handStore} from 'domains/hands'
 
-const PERCENTAGE = 100;
+const PERCENTAGE = 100
 
-projectStore.actions.getAll();
-handStore.actions.getAll();
+projectStore.actions.getAll()
+handStore.actions.getAll()
 
-const projects = projectStore.getters.all;
+const projects = projectStore.getters.all
 
-const activeProjects = computed(() => projects.value.filter(({status}) => status === activeStatus?.id));
+const activeProjects = computed(() => projects.value.filter(({status}) => status === activeStatus?.id))
 
 const hoursActiveProjects = computed(() => {
-    const totalHands = activeProjects.value.map(({hands}) => hands).flat();
+    const totalHands = activeProjects.value.map(({hands}) => hands).flat()
 
     return Math.round(
         totalHands.reduce(
             (acc, curr) => acc + (curr.hours / PERCENTAGE) * handStore.getters.byId(curr.handId).value?.efficiency,
             0,
         ),
-    );
-});
+    )
+})
 
-const MONTHS = 12;
-const WEEKS = 12;
+const MONTHS = 12
+const WEEKS = 12
 
 const totalEfficientHoursNeeded = computed(() =>
     Math.round(
         activeProjects.value.reduce((acc, {hours, totalWeeks, endDate}) => {
-            if (endDate) return acc + hours / totalWeeks;
-            if (!endDate) return acc + (hours * MONTHS) / WEEKS;
-            return acc;
+            if (endDate) return acc + hours / totalWeeks
+            if (!endDate) return acc + (hours * MONTHS) / WEEKS
+            return acc
         }, 0),
     ),
-);
+)
 
 const totalEfficientHoursHands = computed(() => {
-    const totalActiveHands = activeProjects.value.map(({hands}) => hands).flat();
-    const uniqueHands = [...new Set(totalActiveHands.map(hand => hand.handId))];
-    return uniqueHands.reduce((acc, next) => acc + +handStore.getters.byId(next).value?.hours, 0);
-});
+    const totalActiveHands = activeProjects.value.map(({hands}) => hands).flat()
+    const uniqueHands = [...new Set(totalActiveHands.map(hand => hand.handId))]
+    return uniqueHands.reduce((acc, next) => acc + +handStore.getters.byId(next).value?.hours, 0)
+})
 
 const submitLogout = async () => {
     try {
-        await logout();
+        await logout()
     } finally {
-        goToLoginPage();
+        goToLoginPage()
     }
-};
+}
 </script>
 
 <style>
 .router-link-exact-active {
-    background-color: rgba(255, 255, 255, 0.15);
+    background-color: rgba(255, 255, 255, 0.15)
 }
 </style>
 -->
--- resources/js/domains/auth/index.ts
+++ resources/js/domains/auth/index.ts
@@ -1,40 +1,40 @@
-// import {Credentials} from './types';
-// import {PROJECT_DOMAIN_NAME} from 'domains/projects';
-// import {computed, ref} from 'vue';
-// import {getRequest, postRequest} from 'services/http';
+// import {Credentials} from './types'
+// import {PROJECT_DOMAIN_NAME} from 'domains/projects'
+// import {computed, ref} from 'vue'
+// import {getRequest, postRequest} from 'services/http'
 
-// const loggedInUser = ref<User | null>(null);
+// const loggedInUser = ref<User | null>(null)
 
-// export const isLoggedIn = computed(() => loggedInUser.value !== null);
-// export const getLoggedInUser = computed(() => loggedInUser.value);
+// export const isLoggedIn = computed(() => loggedInUser.value !== null)
+// export const getLoggedInUser = computed(() => loggedInUser.value)
 
 // export const login = async (credentials: Credentials) => {
-//     const {data} = await postRequest('login', credentials);
-//     if (!data) return;
-//     loggedInUser.value = data.user;
-// };
+//     const {data} = await postRequest('login', credentials)
+//     if (!data) return
+//     loggedInUser.value = data.user
+// }
 
 // export const logout = async () => {
-//     await getRequest('logout');
-//     loggedInUser.value = null;
-// };
+//     await getRequest('logout')
+//     loggedInUser.value = null
+// }
 
 // export const me = async () => {
-//     const {data} = await getRequest('me');
-//     if (!data) return;
-//     loggedInUser.value = data;
-// };
+//     const {data} = await getRequest('me')
+//     if (!data) return
+//     loggedInUser.value = data
+// }
 
-// export const goToLoginPage = () => goToRoute('Login', undefined);
+// export const goToLoginPage = () => goToRoute('Login', undefined)
 
 // registerBeforeRouteMiddleware(({meta}) => {
 //     if (!isLoggedIn.value && meta?.auth) {
-//         goToLoginPage();
-//         return true;
+//         goToLoginPage()
+//         return true
 //     }
 //     if (isLoggedIn.value && !meta?.canSeeWhenLoggedIn) {
-//         goToOverviewPage(PROJECT_DOMAIN_NAME);
-//         return true;
+//         goToOverviewPage(PROJECT_DOMAIN_NAME)
+//         return true
 //     }
-//     return false;
-// });
+//     return false
+// })
